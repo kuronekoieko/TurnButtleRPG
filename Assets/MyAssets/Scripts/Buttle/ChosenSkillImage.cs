b19035c203dbbcf9230f95b1ca39e2d0f6a04501
ieko <mendo931411.mou@gmail.com>
2019-05-12 17:14:23 +0900

    prefab

---
+++ Assets/MyAssets/Scripts/Buttle/ChosenSkillImage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChosenSkillImage : MonoBehaviour
+{
+    public Text chosenSkillText;
+
+    public void Init(RectTransform parent, Vector3 pos)
+    {
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        //layer0の子にする
+        rectTransform.SetParent(parent);
+        //座標を移動
+        rectTransform.anchoredPosition = pos;
+    }
+
+    public void setSkillText(int skillID)
+    {
+        chosenSkillText.text = DataManager.skillList[skillID].name;
+    }
+}
